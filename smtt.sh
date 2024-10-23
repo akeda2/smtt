285f0dffcec08955fe19e141619add70ce7351cb
akeda <akeda2@gmail.com>
2024-10-23 20:11:46 +0200

    harder to read!

--- smtt.sh
+++ smtt.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # SMT-toggle
+
 CONTROL=/sys/devices/system/cpu/smt/control
-check_state() {
-	SMT_STATE=$(cat /sys/devices/system/cpu/smt/active)
-	echo $SMT_STATE
-}
-[[ $(check_state) -eq 1 ]] && \
-	{ echo "SMT is on!"; echo 'off' | sudo tee $CONTROL ;} || { echo "SMT is off!"; echo 'on' | sudo tee $CONTROL ;}
-echo "SMT is $(check_state)"
+
+check_state() { SMT_STATE=$(cat /sys/devices/system/cpu/smt/active); echo $SMT_STATE ;}
+set_state() { echo "$1" | sudo tee "$CONTROL" > /dev/null && return || false ;}
+we_failed() { echo "FAIL"; exit 1 ;}
+
+[[ $(check_state) -eq 1 ]] \
+	&& { echo "From on..."; set_state 'off' || we_failed ;} \
+	|| { echo "From off...!"; set_state 'on' || we_failed ;}
+[[ $(check_state) -eq 1 ]] \
+	&& { echo "...to on!" ;} \
+	|| { echo "...to off!" ;}
