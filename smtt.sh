88b210f3b5149173d3c6212968d6fe82cf710b5c
akeda <akeda2@gmail.com>
2024-10-24 11:13:46 +0200

    on off get and empty for toggle

--- smtt.sh
+++ smtt.sh
@@ -3,13 +3,15 @@
 
 CONTROL=/sys/devices/system/cpu/smt/control
 
-check_state() { SMT_STATE=$(cat /sys/devices/system/cpu/smt/active); echo $SMT_STATE ;}
+get_state() { SMT_STATE=$(cat /sys/devices/system/cpu/smt/active); echo $SMT_STATE ;}
 set_state() { echo "$1" | sudo tee "$CONTROL" > /dev/null && return || false ;}
 we_failed() { echo "FAIL"; exit 1 ;}
-
-[[ $(check_state) -eq 1 ]] \
-	&& { echo "From on..."; set_state 'off' || we_failed ;} \
-	|| { echo "From off...!"; set_state 'on' || we_failed ;}
-[[ $(check_state) -eq 1 ]] \
+[[ ! -z "$1" ]] && \
+	[[ "$1" == "on" || "$1" == "off" ]] && { set_state "$1" && get_state && exit 0 || exit 1;}
+	[[ "$1" == "get" ]] && { get_state && exit 0 || exit 1;} \
+	|| [[ $(get_state) -eq 1 ]] \
+			&& { echo "From on..."; set_state 'off' || we_failed ;} \
+			|| { echo "From off...!"; set_state 'on' || we_failed ;}
+[[ $(get_state) -eq 1 ]] \
 	&& { echo "...to on!" ;} \
 	|| { echo "...to off!" ;}
