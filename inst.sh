56437c8829524ae0bfd2816afd8dcf1cd4cead1d
akeda <akeda2@gmail.com>
2025-02-25 10:53:18 +0100

    installer update with turbot

--- inst.sh
+++ inst.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo install -m 755 smtt.sh /usr/local/bin/smtt && echo "Install SUCCESS!" || echo "Install FAIL!"
+sudo install -m 755 smtt.sh /usr/local/bin/smtt && echo "smtt install SUCCESS!" || echo "smtt install FAIL!"
+sudo install -m 755 turbot.sh /usr/local/bin/turbot && echo "turbot install SUCCESS!" || echo "turbot install FAIL!"
