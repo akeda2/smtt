5777c42f6182e82a563df1a2640f9f72b9fff35b
akeda <akeda2@gmail.com>
2025-02-25 11:57:20 +0100

    more same output

--- smtt.sh
+++ smtt.sh
@@ -3,7 +3,7 @@
 
 CONTROL=/sys/devices/system/cpu/smt/control
 
-get_state() { SMT_STATE=$(cat /sys/devices/system/cpu/smt/active); echo $SMT_STATE ;}
+get_state() { local SMT_STATE=$(cat /sys/devices/system/cpu/smt/active); echo $SMT_STATE ;}
 set_state() { echo "$1" | sudo tee "$CONTROL" > /dev/null && return || false ;}
 we_failed() { echo "FAIL"; exit 1 ;}
 if [[ ! -z "$1" ]]; then
@@ -23,5 +23,6 @@ if [[ ! -z "$1" ]]; then
 		echo "Usage: smtt on|off|t"; exit
 	fi
 else
-	get_state && exit 0 || exit 1
+	STATE=$(get_state)
+	[[ $STATE == 1 ]] && { echo "SMT on"; exit 0;} || { echo "Turbo off"; exit 1;}
 fi
