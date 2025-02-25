8fec44a9c9dd9547e73dfcca8397176dbbe614d4
akeda <akeda2@gmail.com>
2025-02-25 11:18:05 +0100

    more userfriendly smtt

--- smtt.sh
+++ smtt.sh
@@ -6,12 +6,20 @@ CONTROL=/sys/devices/system/cpu/smt/control
 get_state() { SMT_STATE=$(cat /sys/devices/system/cpu/smt/active); echo $SMT_STATE ;}
 set_state() { echo "$1" | sudo tee "$CONTROL" > /dev/null && return || false ;}
 we_failed() { echo "FAIL"; exit 1 ;}
-[[ ! -z "$1" ]] && \
-	[[ "$1" == "on" || "$1" == "off" ]] && { set_state "$1" && get_state && exit 0 || exit 1;}
-	[[ "$1" == "get" ]] && { get_state && exit 0 || exit 1;} \
-	|| [[ $(get_state) -eq 1 ]] \
+if [[ ! -z "$1" ]]; then
+	if [[ "$1" == "on" || "$1" == "off" ]]; then
+		set_state "$1" && get_state && exit 0 || exit 1
+	elif [[ "$1" == "get" ]]; then
+		get_state && exit 0 || exit 1
+	elif [[ "$1" == "t" ]]; then
+		[[ $(get_state) -eq 1 ]] \
 			&& { echo "From on..."; set_state 'off' || we_failed ;} \
 			|| { echo "From off...!"; set_state 'on' || we_failed ;}
-[[ $(get_state) -eq 1 ]] \
-	&& { echo "...to on!" ;} \
-	|| { echo "...to off!" ;}
+		[[ $(get_state) -eq 1 ]] \
+	        	&& { echo "...to on!" ;} \
+        		|| { echo "...to off!" ;}
+
+	fi
+else
+	echo "Usage: smtt on|off|t"; exit
+fi
