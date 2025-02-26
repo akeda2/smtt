124da9e46645b6aa25f772bf77391c16400d0b28
akeda <akeda2@gmail.com>
2025-02-26 16:25:35 +0100

    cleaner turbot

--- turbot.sh
+++ turbot.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 # Toggle turbo on/off
+# Exits True (0) if turbo is ON
+
 TURBO_OFFSTATE="$(cat /sys/devices/system/cpu/intel_pstate/no_turbo)"
 
 turbo_on(){ echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null;}
 turbo_off(){ echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null;}
-print_status(){
-	TURBO_OFFSTATE="$(cat /sys/devices/system/cpu/intel_pstate/no_turbo)"
-	[[ $TURBO_OFFSTATE == 1 ]] && echo "Turbo off" || echo "Turbo on"
-}
+get_status() { local TURBO_OFFSTATE="$(cat /sys/devices/system/cpu/intel_pstate/no_turbo)" ; echo $TURBO_OFFSTATE ;}
 
 if [[ ! -z $1 ]]; then
 	if [[ $1 == off ]]; then
 		turbo_off
-		print_status
 	elif [[ $1 == on ]]; then
 		turbo_on
-		print_status
 	elif [[ $1 == t ]]; then
-		[[ $TURBO_OFFSTATE == 1 ]] && turbo_on || turbo_off
-		print_status
+		TOGGLE_STATUS=$(get_status); [[ $TOGGLE_STATUS == 1 ]] && turbo_on || turbo_off
 	else
 		echo "Usage: seturbo 'on|off', 't' for toggle. Leave empty for status: 0=off, 1=on"
 	fi
-else
-	[[ $TURBO_OFFSTATE == 0 ]] && { echo "Turbo on"; exit 1 ;} || { echo "Turbo off" ; exit 0 ;}
 fi
+
+RETURN_STATE=$(get_status); [[ $RETURN_STATE == 0 ]] && { echo "Turbo on"; exit 0 ;} || { echo "Turbo off" ; exit 1 ;}
